399a0a90acdc03aa09d5fed1f129a2fb30e4bb3f
Ricardo Arredondo <ricardo.arredondo@sistemasexpertos.cl>
2022-06-26 23:06:00 -0400

    Se agrega loggin de registros

--- app/Http/Controllers/PatenteController.php
+++ app/Http/Controllers/PatenteController.php
@@ -5,30 +5,42 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Vehiculo;
 
+use Exception;
+use Log;
 
 class PatenteController extends Controller
 {
     public function getDataByPatent (Request $request){
         
+        Log::channel("getDataByPatent")->info("Inicio Bucar Datos por patente");
+        Log::channel("getDataByPatent")->info("Request: ".json_encode($request->all()));
+
         $aReqData = array();
         $aReqData["request"]   = isset($request->patent) ? $request->patent : "";
         $aReqData["result"]    = false;
         $aReqData["message"]   = "";
         $aReqData["response"]  = [];
         
-        $vehiculo = Vehiculo::select([
-                                        'patente',
-                                        'tipo_vehiculo AS tipoVehiculo',
-                                        'marca_vehiculo AS marcaVehiculo',
-                                        'modelo_vehiculo AS modeloVehiculo',
-                                        'anio_vehiculo AS anioVehiculo',
-                                        'numero_motor_vehiculo AS numeroMotorVehiculo',
-                                        'rut_propietario AS rutPropietario',
-                                        'nombre_propietario AS nombreApellidoPropietario'
-                                    ])
-                                    ->where("patente","like", "%".$request->patent."%")
-                                    ->take(10)
-                                    ->get();
+        try{
+            $vehiculo = Vehiculo::select([
+                                            'patente',
+                                            'tipo_vehiculo AS tipoVehiculo',
+                                            'marca_vehiculo AS marcaVehiculo',
+                                            'modelo_vehiculo AS modeloVehiculo',
+                                            'anio_vehiculo AS anioVehiculo',
+                                            'numero_motor_vehiculo AS numeroMotorVehiculo',
+                                            'rut_propietario AS rutPropietario',
+                                            'nombre_propietario AS nombreApellidoPropietario'
+                                        ])
+                                        ->where("patente","like", "%".$request->patent."%")
+                                        ->take(10)
+                                        ->get();
+        }catch(Exception $e){
+            $aReqData["message"]   = "Error no controlado";
+            Log::channel("getDataByPatent")->error("Error en la linea: ".$request->getLine());
+            Log::channel("getDataByPatent")->error("Mensaje: ".$request->getMessage());
+            return response()->json($aReqData, 500);
+        }
 
         if(count($vehiculo) <= 0){
             $aReqData["message"]   = "Patente sin resultados";
@@ -37,6 +49,9 @@ class PatenteController extends Controller
 
         $aReqData["result"]   = true;
         $aReqData["response"] = $vehiculo;
+
+        Log::channel("getDataByPatent")->info("Response: ".json_encode($aReqData));
+        Log::channel("getDataByPatent")->info("Fin Bucar Datos por patente");
         return $aReqData;
     }
 }
--- config/logging.php
+++ config/logging.php
@@ -67,6 +67,13 @@ return [
             'days' => 14,
         ],
 
+        'getDataByPatent' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/getDataByPatent/getDataByPatent.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'days' => 14,
+        ],
+
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
